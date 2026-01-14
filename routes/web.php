86441e182efcc0952c97196a938342205ca5bc18
stefanedelman <150808948+stefanedelman@users.noreply.github.com>
2026-01-14 03:57:22 +0100

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Ruta za kreiranje porudžbine (Use Case: Kreiranje porudžbine)
+Route::post('/order', [OrderController::class, 'store']);
 
 
 
