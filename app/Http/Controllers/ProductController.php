44f64ff1119f761204dadad3f4bfc989a4bf729b
stefanedelman <150808948+stefanedelman@users.noreply.github.com>
2026-01-14 04:05:24 +0100

    dodate rute za use case 2.2.2

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,4 +2,55 @@
 
 namespace App\Http\Controllers;
 
-class ProductController extends Controller {}
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * USE CASE 2.2.2: Pregled svih proizvoda
+     * Vraća listu svih proizvoda sa opcionalnom pretragom
+     */
+    public function index(Request $request)
+    {
+        $query = Product::with('category');
+
+        // Pretraga po nazivu (ako je prosleđen search parametar)
+        if ($request->has('search')) {
+            $query->where('name', 'like', '%' . $request->search . '%');
+        }
+
+        $products = $query->get();
+
+        return response()->json([
+            'message' => 'Lista proizvoda',
+            'data' => $products,
+        ]);
+    }
+
+    /**
+     * USE CASE 2.2.2: Filtriranje proizvoda po kategoriji
+     * Vraća proizvode iz određene kategorije
+     */
+    public function byCategory(Category $category)
+    {
+        $products = $category->products;
+
+        return response()->json([
+            'message' => 'Proizvodi iz kategorije: ' . $category->name,
+            'category' => $category->name,
+            'data' => $products,
+        ]);
+    }
+
+    /**
+     * Prikaz pojedinačnog proizvoda
+     */
+    public function show(Product $product)
+    {
+        return response()->json([
+            'data' => $product->load('category'),
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Ruta za kreiranje porudžbine (Use Case: Kreiranje porudžbine)
+// USE CASE 2.2.3: Kreiranje porudžbine
 Route::post('/order', [OrderController::class, 'store']);
+
+// USE CASE 2.2.2: Pregled i pretraga proizvoda
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/category/{category}', [ProductController::class, 'byCategory']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
